16cc3e5b4c2766762ff379066ec007227ce93d30
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-10-19 09:46:55 +1000

    Create TranslateManager.php

---
+++ src/skyss0fly/PocketShield/utils/TranslateManager.php
@@ -0,0 +1,19 @@
+<?php
+namespace skyss0fly\PocketShield;
+use pocketmine\utils\TextFormat as TF;
+
+class TranslateManager {
+
+  public function convert($string string): void {
+$a = "&";
+    $newstr = str_convert($a,$string,TF:FORMAT);
+
+return $newstr;
+    
+  }
+  
+
+
+}
+
+<?

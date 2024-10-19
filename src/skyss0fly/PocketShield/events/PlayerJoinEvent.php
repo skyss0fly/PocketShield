14ad97486e0123818bb5868b24f398dc68c35403
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-10-19 10:08:20 +1000

    Create PlayerJoinEvent.php

---
+++ src/skyss0fly/PocketShield/events/PlayerJoinEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace skyss0fly\PocketShield;
+use pocketmine\event\EventListener;
+use pocketmine\event\Listener;
+
+class PlayerJoinEvent extends EventListener implements Listener {
+
+
+}
+<?

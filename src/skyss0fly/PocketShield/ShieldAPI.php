cd73d906201ea9dcd9d2c4bcf2a5cdd199f31fec
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-11-03 15:15:33 +1000

    Update ShieldAPI

--- src/skyss0fly/PocketShield/ShieldAPI.php
+++ src/skyss0fly/PocketShield/ShieldAPI.php
@@ -2,6 +2,16 @@
 namespace skyss0fly\PocketShield;
 
 class ShieldAPI {
+
+  private const $bedrockPort;
+  private const $javaPort;
+  private const $ipv4;
+  
+public function registerConstants(): {
+$this->$bedrockPort = 19132;
+  $this->$javaPort = 25565;
+  $this->$ipv4 = 0.0.0.0;
+}
 public function getShieldConfig():{
 return $this->getServer()->getConfig();
 
@@ -10,6 +20,16 @@ public function getShieldVersion(): {
   return $this->getShieldConfig("ShieldVer");
   
 }
-  
+public function isDevelopmentBuild(): bool {
+return true;
+}
 
+public function getPocketmineAPIVer(): bool {
+return $this->getShieldConfig("PMAPI");
+}
+
+
+
+  
+}
 <?
